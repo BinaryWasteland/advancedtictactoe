f8ecb12c6411a245fc5708aefcc301ee47710535
gunit.wilson77 <gunit.wilson77@88900c59-204b-8d3e-2f19-f8400ad63edd>
2010-04-12 19:39:27 +0000

    Added TicTacToeLibrary and Location class to the Library

---
+++ AdvancedTicTacToe/AdvancedTicTacToe/TicTacToeLibrary/Locations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToeLibrary
+{
+    [Serializable]
+    public class Locations
+    {
+        public enum LocationID
+        {
+            TopLeft, TopMid, TopRight, 
+            MidLeft, Middle, MidRight,
+            BotLeft, BotMid, BotRight
+        };
+
+        // Member variables used to identify a specific location
+        private LocationID loc;
+
+        //Constructor
+        public Locations(LocationID id)
+        {
+            loc = id;
+        }
+
+        //Accessor methods
+        public LocationID Loc
+        {
+            get { return loc; }
+        }
+    }
+}
