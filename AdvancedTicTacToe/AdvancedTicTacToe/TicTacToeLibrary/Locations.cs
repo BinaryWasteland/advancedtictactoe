960920019f775557e2c934ba9c55c9c58d7d19fc
gunit.wilson77 <gunit.wilson77@88900c59-204b-8d3e-2f19-f8400ad63edd>
2010-04-15 12:47:45 +0000

    Added RemotingConfiguration to the ClientForm Added RegisterCallback, UnregisterCallback, and UpdateGridInfo to Locations

--- AdvancedTicTacToe/AdvancedTicTacToe/TicTacToeLibrary/Locations.cs
+++ AdvancedTicTacToe/AdvancedTicTacToe/TicTacToeLibrary/Locations.cs
@@ -5,16 +5,17 @@ using System.Text;
 
 namespace TicTacToeLibrary
 {
-    [Serializable]
-    public class Locations
+    //[Serializable]
+    public class Locations : MarshalByRefObject
     {
-        private Dictionary<Guid, IUpdatesFromServer> clientCallbacks
-            = new Dictionary<Guid, IUpdatesFromServer>();
+        // Member variables used to identify a specific location
+        private char[] loc;
+        private Dictionary<Guid, IUpdatesFromServer> clientCallbacks = new Dictionary<Guid, IUpdatesFromServer>();
 
-        
+        // Constructor
+        public Locations() { }
 
         //  ----------------- Public methods -----------------
-
         // Each client calls this method after activating the 
         // Shoe object so that the Shoe will have the client's
         // callback information
@@ -32,39 +33,20 @@ namespace TicTacToeLibrary
             clientCallbacks.Remove(token);
         }
 
-        // ----------------- Helper methods -----------------
-
-        // This method is called whenever the Shoe object's
-        // state changes and the client's need to reflect 
-        // the new information in their user interfaces.
-        private void Fire_UpdateShoeInfo()
+        private void UpdateGridInfo()
         {
+            int[] blank = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] x = null;
+            int[] o = null;
             foreach (IUpdatesFromServer callback in clientCallbacks.Values)
-                callback.UpdateLocationCallback( char['A'] /* Value will go here */);
+                callback.UpdateLocationCallback(blank, x, o);
         }
 
-
-
-        //public enum LocationID
-        //{
-        //    TopLeft, TopMid, TopRight, 
-        //    MidLeft, Middle, MidRight,
-        //    BotLeft, BotMid, BotRight
-        //};
-
-        //// Member variables used to identify a specific location
-        //private LocationID loc;
-
-        ////Constructor
-        //public Locations(LocationID id)
-        //{
-        //    loc = id;
-        //}
-
-        ////Accessor methods
-        //public LocationID Loc
-        //{
-        //    get { return loc; }
-        //}
+        //Accessor methods
+        public char[] Grid
+        {
+            get { return loc; }
+            set { loc = value; }
+        }
     }
 }
