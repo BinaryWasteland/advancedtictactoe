c04276a749d60ea1f6ac05a65cab3dbcf6daf7f3
greg.r.jacobs <greg.r.jacobs@88900c59-204b-8d3e-2f19-f8400ad63edd>
2010-04-15 16:22:21 +0000

    Branching off to make game logic seperate from client code

---
+++ branches/GameLogicExodus/TicTacToeClient/UpdatesFromServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TicTacToeLibrary; // IUpdatesFromServer interface
+
+namespace TicTacToeClient
+{
+    public class UpdatesFromServer : MarshalByRefObject, IUpdatesFromServer
+    {
+        private ClientForm client;
+
+        public UpdatesFromServer(ClientForm f)
+        {
+            // Store a ref to the clients user interface
+            client = f;
+        }
+
+        public void UpdateLocationCallback(char[] grid)
+        {
+            client.refreshGrid(grid);
+        }
+    }
+}
