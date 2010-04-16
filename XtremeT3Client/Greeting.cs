e3738cabc40b1a20fe6eff690106f9c781aee9d1
greg.r.jacobs <greg.r.jacobs@88900c59-204b-8d3e-2f19-f8400ad63edd>
2010-04-16 21:37:52 +0000

    Added greeting to get Server and User Name

---
+++ XtremeT3Client/Greeting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XtremeT3Client
+{
+    public partial class Greeting : Form
+    {
+        public XtremeT3Board ParentForm;
+
+        public string UserName
+        {
+            get { return txtName.Text; }
+        }
+
+        public string Server
+        {
+            get { return txtServer.Text; }
+        }
+
+        public Greeting()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            Close(); 
+        }
+
+        private void txtServer_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                DialogResult = DialogResult.OK;
+                btnGo_Click(sender, e);
+            }
+        }
+    }
+}
